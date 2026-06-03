6af15fc96ed53957590f96f7cf42d14dbb7a1896
renoricardo040206 <rricardo01@student.ciputra.ac.id>
2026-06-03 15:13:02 +0800

    Tambah entity dan repo untuk like post

---
+++ src/main/java/com/project/budaya/Entity/PostLikes.java
@@ -0,0 +1,64 @@
+package com.project.budaya.Entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "post_likes")
+public class PostLikes {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Posts post;
+
+    private String createdAt;
+
+    public PostLikes() {
+    }
+
+    public PostLikes(User user, Posts post, String createdAt) {
+        this.user = user;
+        this.post = post;
+        this.createdAt = createdAt;
+    }
+
+    // Getters and Setters
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Posts getPost() {
+        return post;
+    }
+
+    public void setPost(Posts post) {
+        this.post = post;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+}
